78a15a003d65e1fb4fa90fbe1a7a3bc1dd64e00b
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-11 15:09:12 -0600

    super does not work. Not sure exactly what the issue is so I'm starting again from the top

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,7 +2,20 @@ import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = () => {
-    const [someValue, setSomeValue] = useLocalStorage(darkMode)
+    const [dmOn, setDmOn] = useLocalStorage(darkMode)
 
-    
+    useEffect(()=>{
+        if (dmOn === true){
+            document.querySelector('body').classList.add("dark-mode");
+        } else{
+            document.querySelector('body').classList.remove("dark-mode");
+        }
+
+        const toggleMode = e => {
+            e.preventDefault();
+            setDmOn(!darkMode);
+
+        return [storedValue, setValue, toggleMode]
+        
+    }, [setDmOn])
 }
